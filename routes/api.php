5addbc3a34482e4937459ef4f6d72893ba1252b5
Miguel Igdalino <miguel.igdalino@growsari.com>
2023-06-12 02:27:16 +0800

    Added api login and register

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthenticationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('auth/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/auth/register', [AuthenticationController::class, 'createUser']);
+Route::post('/auth/login', [AuthenticationController::class, 'loginUser']);
