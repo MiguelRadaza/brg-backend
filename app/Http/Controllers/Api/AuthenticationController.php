acf3a82b8c6d78c4a9f0773ae21b034fab616b94
Miguel Igdalino <miguel.igdalino@growsari.com>
2023-07-29 23:45:29 +0800

    added bug fix

--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthenticationController extends Controller
 {
+    // login ctr
     public function createUser(Request $request)
     {
         try {
